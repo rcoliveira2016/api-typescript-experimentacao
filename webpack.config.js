7df4776e4bc27bc3d7a94b33e6763ac4505ca75c
Ramon Cardoso de Oliveira <roramon7@gmail.com>
2021-02-23 21:15:05 -0300

    webpack html puging

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,65 @@
 const path = require('path');
 const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = (env, argv) => ({
-  mode: argv.mode === 'development' ? 'development': 'production',
-  entry: {
-    index:'./src/index.ts',
-    sobre:'./src/sobre.ts'
-  },
-  devtool: argv.mode === 'development' ? 'inline-source-map' : false,
-  output: {
-    path: path.resolve(__dirname, 'public/js'),
-    publicPath: '/js/dist',
-    filename: '[name].bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },      
-    ],
-  },
-  resolve: {
-    extensions: [ '.ts', '.js' ],
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    compress: true,
-    port: 9000
+class ConfiguracaoPagina{
+  constructor(fileEntry, keyEntry, nomePagina, titulo){
+    this.fileEntry = fileEntry;
+    this.keyEntry = keyEntry;
+    this.nomePagina = nomePagina;
+    this.titulo = titulo;
   }
-})
+}
+
+var paginas = [
+  new ConfiguracaoPagina('./src/index.ts','index', 'index'),
+  new ConfiguracaoPagina('./src/sobre.ts','sobre', 'sobre'),
+]
+
+module.exports = (env, argv) => {
+  var config = ({
+    mode: argv.mode === 'development' ? 'development': 'production',
+    entry: {},
+    devtool: argv.mode === 'development' ? 'inline-source-map' : false,
+    output: {
+      path: path.resolve(__dirname, 'public/js'),
+      publicPath: '/js/dist',
+      filename: '[name].bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },      
+      ],
+    },
+    plugins:[],
+    resolve: {
+      extensions: [ '.ts', '.js' ],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      compress: true,
+      port: 9000
+    }
+  })
+
+  paginas.forEach(pagina=>{
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        hash: true,
+        filename: path.resolve(__dirname, `public/${pagina.nomePagina}.html`),
+        template: './src/templates/html/page.defalt.html',
+        chunks:[
+          pagina.keyEntry
+        ]
+      })
+    );
+
+    config.entry[pagina.keyEntry] = pagina.fileEntry;
+  });
+
+  return config;
+}
